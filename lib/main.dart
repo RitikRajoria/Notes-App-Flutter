262642e30cea8e08e735501eadbfc3fcdd943987
Ritik Rajoria <ritikrajrrr@gmail.com>
2022-05-17 13:49:29 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app_with_bloc/screens/loginPage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: LoginPage(),
+    );
+  }
+}
