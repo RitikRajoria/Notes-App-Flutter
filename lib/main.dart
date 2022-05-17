c22b2d5a7382a77792d788b470aa2427dcb65966
Ritik Rajoria <ritikrajrrr@gmail.com>
2022-05-17 14:13:13 +0530

    setup firebase , added packages, created App theme file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:notes_app_with_bloc/screens/loginPage.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
---
+++ lib/theme/theme.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTheme {
+  static Color mainColor = Color(0xff000633);
+  static Color bgColor = Color(0xffe2e2ff);
+  static Color accentColor = Color(0xff0065ff);
+
+  //card
+  static List<Color> cardsColor = [
+    Colors.white,
+    Colors.red.shade100,
+    Colors.pink.shade100,
+    Colors.yellow.shade100,
+    Colors.purple.shade100,
+    Colors.orange.shade100,
+    Colors.blue.shade100,
+    Colors.green.shade100,
+    Colors.blueGrey.shade200,
+  ];
+
+  //textStyle
+
+  static TextStyle mainTitle =
+      GoogleFonts.roboto(fontSize: 18.0, fontWeight: FontWeight.bold);
+  static TextStyle mainContent =
+      GoogleFonts.roboto(fontSize: 16.0, fontWeight: FontWeight.normal);
+  static TextStyle dateTitle =
+      GoogleFonts.roboto(fontSize: 13.0, fontWeight: FontWeight.w500);
+}
