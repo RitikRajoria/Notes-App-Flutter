ca6b2c59e50e433837e45e1cdbc732738927d4f5
RitikRajoria <ritikrajrrr@gmail.com>
2022-07-01 18:23:09 +0530

    minor ui fix

--- lib/screens/loginPage.dart
+++ lib/screens/loginPage.dart
@@ -19,84 +19,86 @@ class _LoginPageState extends State<LoginPage> {
     final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.grey.shade900,
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 50,
-              ),
-              Text(
-                'LOGIN',
-                style: GoogleFonts.lato(
-                  fontSize: 38,
-                  letterSpacing: 4,
-                  fontWeight: FontWeight.bold,
-                  wordSpacing: 1.9,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 50,
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: size.width,
-                  height: size.width,
-                  child: Image.asset('assets/images/login.png'),
+                Text(
+                  'LOGIN',
+                  style: GoogleFonts.lato(
+                    fontSize: 38,
+                    letterSpacing: 4,
+                    fontWeight: FontWeight.bold,
+                    wordSpacing: 1.9,
+                  ),
                 ),
-              ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 18, top: 12),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
                   child: Container(
-                    child: Text(
-                      "Create And Manage Your Notes",
-                      style: GoogleFonts.lato(
-                          fontSize: 28, fontWeight: FontWeight.w500),
+                    width: size.width,
+                    height: size.width,
+                    child: Image.asset('assets/images/login.png'),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 18, top: 12),
+                    child: Container(
+                      child: Text(
+                        "Create And Manage Your Notes",
+                        style: GoogleFonts.lato(
+                            fontSize: 28, fontWeight: FontWeight.w500),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: (size.height) * 0.06,
-              ),
-              Container(
-                height: 75,
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Color.fromARGB(255, 170, 137, 229),
-                    ),
-                    onPressed: () {
-                      final provider = Provider.of<GoogleSignInProvider>(
-                          context,
-                          listen: false);
-                      provider.googleLogin(context);
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        FaIcon(
-                          FontAwesomeIcons.google,
-                          size: 20,
-                        ),
-                        SizedBox(
-                          width: (size.width) * 0.067,
-                        ),
-                        Text(
-                          "Continue with Google",
-                          style: GoogleFonts.lato(
-                              fontSize: 18, fontWeight: FontWeight.w800),
-                        ),
-                      ],
+                SizedBox(
+                  height: (size.height) * 0.06,
+                ),
+                Container(
+                  height: 75,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Color.fromARGB(255, 170, 137, 229),
+                      ),
+                      onPressed: () {
+                        final provider = Provider.of<GoogleSignInProvider>(
+                            context,
+                            listen: false);
+                        provider.googleLogin(context);
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          FaIcon(
+                            FontAwesomeIcons.google,
+                            size: 20,
+                          ),
+                          SizedBox(
+                            width: (size.width) * 0.067,
+                          ),
+                          Text(
+                            "Continue with Google",
+                            style: GoogleFonts.lato(
+                                fontSize: 18, fontWeight: FontWeight.w800),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
